6f3c794a67c2f35b608bfb4303f165ff4e16cd54
JosiahKou <155879792+JosiahKou@users.noreply.github.com>
2026-04-27 05:38:39 +0000

    fix

--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,6 @@
-// Originally t.c from Program 2.4
-int a = 1, b = 2, c = 0;
+int a = 1, b = 2, c = 1;  // Force into data section
 
-int sum(int x, int y) { 
-    return x + y; 
+int sum(int x, int y) {
+    c = 0;  // Set to 0 at runtime
+    return x + y;
 }
